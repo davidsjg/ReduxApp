c865247204ecfc8eebfb5b36ee42dcde456ed4e4
Joey Davidson <j.davidson18@gmail.com>
2021-10-18 12:21:52 -0600

    state updating using dispatch, app working on single page

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,18 @@ function App() {
   const isLogged = useSelector((state) => state.isLogged);
   const dispatch = useDispatch();
 
+  function handleClick1() {
+    dispatch(increment());
+  }
+  function handleClick2() {
+    dispatch(decrement());
+  }
   return (
     <>
       <h1>Counter {counter}</h1>
       {/* <button onClick={() => dispatch(increment()))}>+</button> */}
-      <button>+</button>
-      <button>-</button>
+      <button onClick={handleClick1}>+</button>
+      <button onClick={handleClick2}>-</button>
 
       {isLogged ? <h5>User is Logged In</h5> : <h5>User is not logged in</h5>}
     </>
