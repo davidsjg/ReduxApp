138381c27b46c1bd29f1ed72df9deb0d1a50e144
Joey Davidson <j.davidson18@gmail.com>
2021-10-18 12:49:25 -0600

    front end functioning

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,7 @@
 import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./actions";
 
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const isLogged = useSelector((state) => state.isLogged);
-  const dispatch = useDispatch();
-
-  function handleClick1() {
-    dispatch(increment());
-  }
-  function handleClick2() {
-    dispatch(decrement());
-  }
-  return (
-    <>
-      <h1>Counter {counter}</h1>
-      {/* <button onClick={() => dispatch(increment()))}>+</button> */}
-      <button onClick={handleClick1}>+</button>
-      <button onClick={handleClick2}>-</button>
-
-      {isLogged ? <h5>User is Logged In</h5> : <h5>User is not logged in</h5>}
-    </>
-  );
+  return <h1>Hello YALLLLL</h1>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
-import allReducer from "./reducers";
-import { Provider } from "react-redux";
-
-const store = createStore(
-  allReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
 
   document.getElementById("root")
 );
