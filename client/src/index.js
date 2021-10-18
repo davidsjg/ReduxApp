984d978a3d12af0e6d7a7fc37e39e220a28b3274
Joey Davidson <j.davidson18@gmail.com>
2021-10-18 09:52:02 -0600

    reducers combined, displayed in redux devtools extension

--- client/src/index.js
+++ client/src/index.js
@@ -2,43 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
 import { createStore } from "redux";
+import allReducer from "./reducers";
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//STORE -> GLOBALIZED STATE
-
-//ACTION INCREMENT - function that returns an object
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-//REDUCER - DESCRIBES HOW ACTIONS TRANSFORMS STATE INTO NEXT STATE
-//checks action, and based on action it changes store
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-
-let store = createStore(counter);
-
-//display in console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
