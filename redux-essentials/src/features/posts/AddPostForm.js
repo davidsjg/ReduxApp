8b83fdf7136411a20882953a9ec5b2b45c951ba1
Joey Davidson <j.davidson18@gmail.com>
2021-11-17 12:19:33 -0700

    added post form, captured user inputs and stored in local state

---
+++ redux-essentials/src/features/posts/AddPostForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        {/* htmlFor refers to the id of the element this label is associated with */}
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <input
+          type="text"
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button">Save Post</button>
+      </form>
+    </section>
+  );
+};
